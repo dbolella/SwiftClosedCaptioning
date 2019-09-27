2524aaf37c4ad04d8269488744ec6f328ea64490
Danny Bolella <dbolella@gmail.com>
2019-09-27 14:03:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+The latest version of SFSpeechRecognizer allows offline speech-to-text, amongst other new features.  This project builds on the work of other devs to create a demo of how closed captioning could work with video playback.
+
+The benefits of offline transcription is no more data usage, no more need for a data connection at all, and unlimited usage (Apple restricted transcription to 1-minute at a time and for limited hits per day).  The downside is that it may not be as accurate nor have the benefit of a constantly learning cloud.
+
+The potential applications, however, are huge for Accessibility purposes.  Combined with CoreML/CreateML Speech capabilities, it can potentially open up a number of use cases.
+
+SUPER CREDITS:
+Essentially forked from https://github.com/peacemoon/SFSpeechRecognizerRealtimeVideoCaptioning, which was forked from https://github.com/zats/SpeechRecognition, and uses the Swift version of the Audio Processor found in https://github.com/gchilds/MTAudioProcessingTap-in-Swift.
+
+Phew.
---
+++ RealtimeCaptioning/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  RealtimeCaptioning
+//
+//  Created by An Tran on 08.05.19.
+//  Copyright © 2019 An Tran. All rights reserved.
+//
+
+import UIKit
+import Speech
+import AVFoundation
+import Foundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var captionLabel: UILabel!
+
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+
+
+    private let playerLayer = AVPlayerLayer()
+    
+    private var vmInput: VideoMediaInput!
+    private var isPlaying = true
+
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var captionsLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Asset
+        guard let url = Bundle.main.url(forResource: "video2", withExtension: "mp4") else {
+            print("can't get url")
+            return
+        }
+        
+        vmInput = VideoMediaInput(url: url, delegate: self)
+
+        // Player view
+        let playerView: UIView! = view
+        playerLayer.player = vmInput.player
+        playerLayer.frame = playerView.bounds
+        playerView.layer.insertSublayer(playerLayer, at: 0)
+
+        self.setupRecognition()
+    }
+    @IBAction func didTapPermissionButton(_ sender: UIButton) {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            switch authStatus {
+            case .authorized:
+                print("Speech recognition authorized")
+            case .denied:
+                print("Speech recognition authorization denied")
+            case .restricted:
+                print("Not available on this device")
+            case .notDetermined:
+                print("Not determined")
+            }
+        }
+    }
+
+    @IBAction func didTapPlayButton(_ sender: UIButton) {
+        print("Start playing....")
+        if (isPlaying) {
+            vmInput.pauseVideo()
+            playButton.titleLabel?.text = "Play"
+        }else {
+            vmInput.playVideo()
+            playButton.titleLabel?.text = "Pause"
+        }
+        isPlaying.toggle()
+    }
+
+    private func setupRecognition() {
+        let recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        // we want to get continuous recognition and not everything at once at the end of the video
+        recognitionRequest.shouldReportPartialResults = true
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { [weak self] result, error in
+            self?.captionLabel.text = result?.bestTranscription.formattedString
+
+            // if connected to internet, then once in about every minute recognition task finishes
+            // so we need to set up a new one to continue recognition
+            if result?.isFinal == true {
+                self?.recognitionRequest = nil
+                self?.recognitionTask = nil
+
+                self?.setupRecognition()
+            }
+        }
+        self.recognitionRequest = recognitionRequest
+    }
+
+    override func viewDidLayoutSubviews() {
+        playerLayer.frame = view.bounds
+    }
+}
+
+extension ViewController: VideoMediaInputDelegate {
+    func videoFrameRefresh(sampleBuffer: CMSampleBuffer) {
+        recognitionRequest?.appendAudioSampleBuffer(sampleBuffer)
+    }
+}
